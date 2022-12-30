057a5ddaab588a26373ab58dcfa1042720eadbff
km3140 <kimn3140@naver.com>
2022-12-30 19:14:49 +0900

    lec04

---
+++ vscode-setup/.eslintrc.js
@@ -0,0 +1,4 @@
+//
+module.exports = {
+  extends: ['airbnb-base', 'plugin:node/recommended', 'prettier'], // prettier가 맨 뒤에 와야 함
+};
---
+++ vscode-setup/main.js
@@ -0,0 +1,18 @@
+// 다음줄만 eslint의 모든 검사 무시
+/* eslint-disable-next-line */
+console.log('hello eslint');
+
+// 모든 룰을 꺼버리는 것은 좋은 습관이 아니다
+// 검사 해야할 다른 규칙도 무시되기 때문에 아래와 같은 상황이 있을 수 있음
+/* eslint-disable-next-line */
+console.log(eval());
+
+// 올바른 예시
+/* eslint-disable-next-line no-console */
+console.log(eval());
+
+// airbnb-base에 포함된 no-var-rule
+// var 사용 시 자동으로 const로 바뀜
+
+// eslint-plugin-node
+exports = 3;
